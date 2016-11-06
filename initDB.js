765efaa90ca32f2718174d886721a2e9cf3cf97e
Andrew <andrewchellis@users.noreply.github.com>
2016-11-06 01:31:48 -0500

    Backend updates

--- initDB.js
+++ initDB.js
@@ -3,7 +3,7 @@ var db = new sqlite3.Database('votewithfriends.db');
 
 db.serialize(function() {
     db.run("CREATE TABLE drivers (ID INTEGER PRIMARY KEY,NAME TEXT, POLLINGLOC TEXT, TIME INTEGER, SEATS INTEGER)");
-    db.run("CREATE TABLE friends (ID INTEGER PRIMARY KEY,NAME TEXT,DRIVER TEXT, FOREIGN KEY(DRIVER) REFERENCES drivers(NAME))");
+    db.run("CREATE TABLE friends (ID INTEGER PRIMARY KEY,NAME TEXT,DRIVER INTEGER, FOREIGN KEY(DRIVER) REFERENCES drivers(ID))");
     db.run("CREATE TABLE locations (location TEXT, startTime INTEGER, stopTime Integer)");
     //var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
     //for(var i=0;i<10;i++){
