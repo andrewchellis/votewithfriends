23e0fd398c9819fb58589bd935d7fdd69ca3323a
achellis <admin@VotewithFriends.org>
2016-11-06 08:42:03 +0000

    Updated db script to be good

--- initDB.js
+++ initDB.js
@@ -2,7 +2,7 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('votewithfriends.db');
 
 db.serialize(function() {
-    db.run("CREATE TABLE drivers (ID INTEGER,NAME TEXT, POLLINGLOC TEXT, TIME INTEGER, SEATS INTEGER, PRIMARY KEY(ID) ON CONFLICT FAIL)");
+    db.run("CREATE TABLE drivers (ID INTEGER,NAME TEXT, POLLINGLOC TEXT, TIME TEXT, SEATS TEXT, PRIMARY KEY(ID) ON CONFLICT FAIL)");
     db.run("CREATE TABLE friends (ID INTEGER,NAME TEXT,DRIVER INTEGER, PRIMARY KEY(ID) ON CONFLICT FAIL, FOREIGN KEY(DRIVER) REFERENCES drivers(ID))");
     db.run("CREATE TABLE locations (location TEXT, startTime INTEGER, stopTime Integer, PRIMARY KEY(location) ON CONFLICT FAIL)");
     //    stmt.run("Ipsum "+i);
