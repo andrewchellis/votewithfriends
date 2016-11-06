fd45ac5f7321351197a95990b583b6ffbe51d2b7
Samuel David Bravo <samueldavidbravo@gmail.com>
2016-11-06 01:29:11 -0400

    hide cards

--- fbScripts.js
+++ fbScripts.js
@@ -15,16 +15,14 @@ function statusChangeCallback(response) {
     // for FB.getLoginStatus().
     if (response.status === 'connected') {
       // Logged into your app and Facebook.
+      loginworks();
+
       testAPI();
     } else if (response.status === 'not_authorized') {
       // The person is logged into Facebook, but not your app.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into this app.';
     } else {
       // The person is not logged into Facebook, so we're not sure if
       // they are logged into this app or not.
-      document.getElementById('status').innerHTML = 'Please log ' +
-        'into Facebook.';
     }
   }
 
